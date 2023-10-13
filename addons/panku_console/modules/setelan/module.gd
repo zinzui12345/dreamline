efb9b685f3f751a58c493cff5a587ae54dd10986
zinzui12345 <48501018+zinzui12345@users.noreply.github.com>
2023-10-13 21:05:25 +0800

    optimize VRAM usage

--- addons/panku_console/modules/setelan/module.gd
+++ addons/panku_console/modules/setelan/module.gd
@@ -20,8 +20,8 @@ func init_module():
 
 func quit_module():
 	# save settings
-	save_module_data("lynx_window_blur_effect", get_module_opt().window_blur_effect)
-	save_module_data("lynx_window_base_color", get_module_opt().window_color)
+	#save_module_data("lynx_window_blur_effect", get_module_opt().window_blur_effect)
+	#save_module_data("lynx_window_base_color", get_module_opt().window_color)
 	save_module_data("lynx_window_enable_os_window", get_module_opt().enable_os_window)
 	save_module_data("lynx_window_os_window_bg_color", get_module_opt().os_window_bg_color)
 	
