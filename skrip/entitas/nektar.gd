1dbd3ecc66b863a32b32d7334b83cc3b87060396
zinzui12345 <48501018+zinzui12345@users.noreply.github.com>
2023-12-20 20:16:31 +0800

    fix nectar desync

--- skrip/entitas/nektar.gd
+++ skrip/entitas/nektar.gd
@@ -2,3 +2,31 @@
 extends RigidBody3D
  
 class_name nektar
+
+func _ready(): call_deferred("_setup")
+func _setup():
+	if get_parent().get_path() != server.permainan.dunia.get_node("entitas").get_path():
+		if server.permainan.koneksi == Permainan.MODE_KONEKSI.SERVER:
+			server._tambahkan_entitas(
+				"res://skena/entitas/nektar.scn",
+				global_transform.origin,
+				rotation,
+				[]
+			)
+		queue_free()
+
+@onready var posisi_awal = global_transform.origin
+@onready var rotasi_awal = rotation
+
+func _physics_process(delta):
+	if global_position.y < server.permainan.batas_bawah: # atur ulang kalau jatuh ke void
+		if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
+			freeze = true
+			angular_velocity = Vector3.ZERO
+			linear_velocity  = Vector3.ZERO
+			global_transform.origin = posisi_awal
+			rotation		 		= rotasi_awal
+			Panku.notify("re-spawn "+name)
+			freeze = false
+		elif server.permainan.koneksi == Permainan.MODE_KONEKSI.CLIENT:
+			server.rpc_id(1, "_sesuaikan_posisi_entitas", name, client.id_koneksi)
