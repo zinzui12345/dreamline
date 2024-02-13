c87b1e8ccac74067b38c026089ae639a034b1978
zinzui12345 <48501018+zinzui12345@users.noreply.github.com>
2024-02-13 15:37:59 +0800

    dynamic player avatar

--- skrip/tampilan_karakter.gd
+++ skrip/tampilan_karakter.gd
@@ -1,28 +1,47 @@
-extends SubViewport
+extends SubViewportContainer
 
 var target_karakter : Karakter
 
 func _enter_tree():
 	set_physics_process(false)
 func _ready():
-	$arah/latar.visible = false
+	visible = false
+	$SubViewport/arah/latar.visible = false
 
 func dapatkan_tampilan(karakter : Karakter) -> Image:
 	# root.dunia.get_node("tampilan_karakter").dapatkan_tampilan(root.karakter)
+	# root._atur_daftar_pemain(1, "gambar", root.dunia.get_node("tampilan_karakter").dapatkan_tampilan(root.karakter))
 	if is_instance_valid(karakter):
-		$arah.global_position = karakter.get_node("%GeneralSkeleton/kepala").global_position
+		visible = true
 		target_karakter = karakter
-		set_physics_process(true)
-		$arah/latar.visible = true
-		await RenderingServer.frame_post_draw
-		var hasil = get_texture().get_image()
-		set_physics_process(false)
-		#$arah/latar.visible = false
+		#set_physics_process(true)
+		if target_karakter.id_pemain == client.id_koneksi: get_tree().paused = true
+		$lantai/CollisionShape3D.disabled = false
+		$SubViewport/arah/latar.visible = true
+		$lantai.global_position = karakter.global_position
+		$SubViewport/arah.global_position = karakter.global_position
+		$SubViewport/arah.global_rotation.y = karakter.global_rotation.y
+		$SubViewport/arah/latar.position.z = -karakter.get_node("pengamat").position.y
+		$SubViewport/arah/kamera.position.z = -karakter.get_node("pengamat").position.y
+		#await RenderingServer.frame_post_draw
+		await get_tree().create_timer(0.5).timeout
+		var hasil : Image = $SubViewport.get_texture().get_image()
+		#$SubViewport.get_texture().get_image().save_png("user://uji_pp.png")
+		$SubViewport/arah/latar.visible = false
+		$lantai/CollisionShape3D.disabled = true
+		if target_karakter.id_pemain == client.id_koneksi: get_tree().paused = false
+		#set_physics_process(false)
+		visible = false
 		target_karakter = null
+		Panku.notify("mengatur gambar ikon karakter")
 		return hasil
 	else:
 		return Image.new()
 
 func _physics_process(_delta):
 	if is_instance_valid(target_karakter):
-		$arah.global_position = target_karakter.get_node("%GeneralSkeleton/kepala").global_position
+		$lantai.global_position = target_karakter.global_position
+		$SubViewport/arah.global_rotation.y = target_karakter.global_rotation.y
+		$SubViewport/arah.global_rotation.y = target_karakter.global_rotation.y
+		$SubViewport/arah/latar.position.z = -target_karakter.get_node("pengamat").position.y
+		$SubViewport/arah/kamera.position.z = -target_karakter.get_node("pengamat").position.y
