d3e2013f1c44369e4ef564f8d058a425140e9290
zinzui12345 <48501018+zinzui12345@users.noreply.github.com>
2024-04-24 15:29:26 +0800

    fix dialog character_names invalid on server

--- skrip/entitas/dialog_informasi.gd
+++ skrip/entitas/dialog_informasi.gd
@@ -7,21 +7,39 @@ var sinkron_kondisi = [["properti_dialog", {}]]
 @export var data_dialog : DialogueResource : 
 	set(data):
 		if data != null:
+			#print_debug(data.titles)
+			#print_debug(data.first_title)
+			#print_debug(data.character_names)
+			#print_debug(data.lines)
 			properti_dialog = {
 				"titles": data.titles,
 				"first_title": data.first_title,
 				"character_names": data.character_names,
 				"lines": data.lines
 			}
+		# debug : keknya mending pindah di setelah entitas di-instantiate() deh
+		#server.permainan.dunia.print_tree_pretty()
 		data_dialog = data
 var dialog : DialogueResource
 var properti_dialog : Dictionary : 
 	set(data):
 		var tmp_dialog : DialogueResource = DialogueResource.new()
-		tmp_dialog.titles = data["titles"]
-		tmp_dialog.character_names = data["character_names"]
-		tmp_dialog.first_title = data["first_title"]
-		tmp_dialog.lines = data["lines"]
+		var indeks_baris = data.lines.keys()
+		tmp_dialog.titles = data.titles
+		tmp_dialog.character_names = data.character_names
+		tmp_dialog.first_title = data.first_title
+		# karena array kosong yang tersimpan di dictionary berformat default, atur format secara manual
+		var tmp_arr_dict : Array[Dictionary] = []
+		for baris in data.lines.size():
+			if data.lines[indeks_baris[baris]].get("character_replacements") != null:
+				if typeof(data.lines[indeks_baris[baris]]["character_replacements"]) == TYPE_ARRAY and data.lines[indeks_baris[baris]]["character_replacements"].size() == 0:
+					data.lines[indeks_baris[baris]]["character_replacements"] = tmp_arr_dict
+			if data.lines[indeks_baris[baris]].get("text_replacements") != null:
+				if typeof(data.lines[indeks_baris[baris]]["text_replacements"]) == TYPE_ARRAY and data.lines[indeks_baris[baris]]["text_replacements"].size() == 0:
+					data.lines[indeks_baris[baris]]["text_replacements"] = tmp_arr_dict
+			if data.lines[indeks_baris[baris]].get("character") != null and data.lines[indeks_baris[baris]].get("character") != "" and !data.character_names.has(data.lines[indeks_baris[baris]].character):
+				data.character_names.append(data.lines[indeks_baris[baris]].character)
+		tmp_dialog.lines = data.lines
 		dialog = tmp_dialog
 		sinkron_kondisi[0][1] = data
 		properti_dialog = data
