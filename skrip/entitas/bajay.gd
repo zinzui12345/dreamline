f10fccb4ff2942644b49520625b45981f4ebae9c
zinzui12345 <48501018+zinzui12345@users.noreply.github.com>
2024-09-13 09:22:14 +0800

    add bajaj controller

--- skrip/entitas/bajay.gd
+++ skrip/entitas/bajay.gd
@@ -6,9 +6,43 @@ const sinkron_kondisi = [
 	["warna_2", Color("3900ff")],
 	["warna_3", Color("ff0021")],
 	["warna_4", Color("ff5318")],
-	["warna_5", Color("000000")]
+	["warna_5", Color("000000")],
+	["id_pengemudi", -1]
 ]
 const jalur_instance = "res://skena/entitas/bajay.tscn"
+const batas_putaran_stir = 0.5
+
+var id_pengemudi = -1:
+	set(id):
+		if id != -1:
+			server.permainan.dunia.get_node("pemain/"+str(id)).set("gestur", "duduk")
+			server.permainan.dunia.get_node("pemain/"+str(id)).set("pose_duduk", "mengemudi")
+			
+			# otomatis set ketika pos_tangan tidak valid kemudian ready(), skrip pada pos_tangan
+			if server.permainan.dunia.get_node_or_null("pemain/"+str(id)+"/%tangan_kanan") != null:
+				server.permainan.dunia.get_node("pemain/"+str(id)+"/%tangan_kanan").set_target_node($setir/rotasi_stir/pos_tangan_kanan.get_path())
+				server.permainan.dunia.get_node("pemain/"+str(id)+"/%tangan_kanan").start()
+			if server.permainan.dunia.get_node_or_null("pemain/"+str(id)+"/%tangan_kiri") != null:
+				server.permainan.dunia.get_node("pemain/"+str(id)+"/%tangan_kiri").set_target_node($setir/rotasi_stir/pos_tangan_kiri.get_path())
+				server.permainan.dunia.get_node("pemain/"+str(id)+"/%tangan_kiri").start()
+			#call("add_collision_exception_with", server.permainan.dunia.get_node("pemain/"+str(id)))
+		else:
+			if server.permainan.dunia.get_node("pemain").get_node_or_null(str(id_pengemudi)) != null:
+				server.permainan.dunia.get_node("pemain/"+str(id_pengemudi)).set("gestur", "berdiri")
+				
+				server.permainan.dunia.get_node("pemain/"+str(id_pengemudi)+"/%tangan_kanan").set_target_node("")
+				server.permainan.dunia.get_node("pemain/"+str(id_pengemudi)+"/%tangan_kanan").stop()
+				server.permainan.dunia.get_node("pemain/"+str(id_pengemudi)+"/%tangan_kiri").set_target_node("")
+				server.permainan.dunia.get_node("pemain/"+str(id_pengemudi)+"/%tangan_kiri").stop()
+		id_pengemudi = id
+var arah_kemudi : Vector2
+var arah_belok : float
+var kecepatan_normal = 80
+var kecepatan_maksimal = 150
+var kecepatan_mundur = 60
+var kekuatan_mesin = kecepatan_normal
+var kekuatan_rem = 3
+var kecepatan_laju = 0
 
 var mat1 : StandardMaterial3D
 var mat2 : StandardMaterial3D
@@ -56,5 +90,86 @@ func mulai() -> void:
 	$model/detail/bodi.set_surface_override_material(11, mat5)
 	$model/detail/subreker_depan.set_surface_override_material(0, mat5)
 # fungsi yang akan dipanggil setiap saat menggantikan _process(delta)
-func proses(_waktu_delta : float) -> void:
-	pass
+func proses(waktu_delta : float) -> void:
+	if id_pengemudi == multiplayer.get_unique_id():
+		server.permainan.dunia.get_node("pemain/"+str(id_pengemudi)).global_position = global_position
+		server.permainan.dunia.get_node("pemain/"+str(id_pengemudi)).rotation = rotation
+		
+		if arah_kemudi.y > 0:	set("engine_force", kekuatan_mesin * arah_kemudi.y)
+		elif arah_kemudi.y < 0:	set("engine_force", kecepatan_mundur * arah_kemudi.y)
+		else:					set("engine_force", arah_kemudi.y)
+		
+		kecepatan_laju = self.linear_velocity * transform.basis
+		
+		if arah_kemudi.x != 0:
+			arah_belok = arah_kemudi.x * batas_putaran_stir
+		else:
+			arah_belok = 0
+		
+		self.steering = move_toward(self.steering, arah_belok, 1.5 * waktu_delta)
+		$setir/rotasi_stir.rotation_degrees.y = self.steering * 110
+func _input(_event): # lepas walaupun tidak di-fokus
+	if id_pengemudi == multiplayer.get_unique_id() and server.permainan.dunia.get_node("pemain/"+str(id_pengemudi)).kontrol:
+		if Input.is_action_just_pressed("aksi1"):		Panku.notify("bip!")
+		if Input.is_action_just_pressed("aksi2"):		await get_tree().create_timer(0.1).timeout; server.gunakan_entitas(name, "_lepas")
+		
+		if Input.is_action_pressed("maju"): 	arah_kemudi.y = Input.get_action_strength("maju")
+		elif Input.is_action_pressed("mundur"):	arah_kemudi.y = -Input.get_action_strength("mundur")
+		else:									arah_kemudi.y = 0
+		
+		if Input.is_action_pressed("kiri"):		arah_kemudi.x = Input.get_action_strength("kiri")
+		elif Input.is_action_pressed("kanan"):	arah_kemudi.x = -Input.get_action_strength("kanan")
+		else:									arah_kemudi.x = 0
+		
+		if Input.is_action_pressed("lompat"):	self.brake = Input.get_action_strength("lompat")
+		else:									self.brake = 0
+
+func fokus():
+	server.permainan.set("tombol_aksi_2", "angkat_sesuatu")
+func gunakan(id_pemain):
+	if id_pengemudi == id_pemain:					server.gunakan_entitas(name, "_lepas")
+	elif id_pengemudi == -1: 						server.gunakan_entitas(name, "_kemudikan")
+func hapus(): # ketika tampilan dihapus
+	if id_pengemudi != -1 and server.permainan.dunia.get_node("pemain").get_node_or_null(str(id_pengemudi)) != null:
+		server.permainan.dunia.get_node("pemain/"+str(id_pengemudi)+"/%tangan_kanan").set_target_node("")
+		server.permainan.dunia.get_node("pemain/"+str(id_pengemudi)+"/%tangan_kanan").stop()
+		server.permainan.dunia.get_node("pemain/"+str(id_pengemudi)+"/%tangan_kiri").set_target_node("")
+		server.permainan.dunia.get_node("pemain/"+str(id_pengemudi)+"/%tangan_kiri").stop()
+	queue_free()
+
+func _kemudikan(id):
+	if id == client.id_koneksi:
+		server.permainan.dunia.get_node("pemain/"+str(id))._atur_penarget(false)
+		await get_tree().create_timer(0.05).timeout		# ini untuk mencegah fungsi !_target di _process()
+		server.permainan.set("tombol_aksi_1", "lempar_sesuatu")
+		server.permainan.get_node("kontrol_sentuh/aksi_1").visible = true
+		server.permainan.get_node("hud/bantuan_input/aksi1").visible = true
+		server.permainan.set("tombol_aksi_2", "jatuhkan_sesuatu")
+		server.permainan.get_node("kontrol_sentuh/aksi_2").visible = true
+		server.permainan.get_node("hud/bantuan_input/aksi2").visible = true
+		
+		# ubah pemroses pada server
+		var tmp_kondisi = [["id_proses", id], ["id_pengemudi", id]]
+		if server.permainan.koneksi == Permainan.MODE_KONEKSI.SERVER: server._sesuaikan_kondisi_entitas(id_proses, name, tmp_kondisi)
+		else: server.rpc_id(1, "_sesuaikan_kondisi_entitas", id_proses, name, tmp_kondisi)
+	# atur id_pengemudi dan id_proses
+	id_pengemudi = id
+	id_proses = id
+func _lepas(id):
+	#if server.permainan.dunia.get_node("pemain").get_node_or_null(str(id)) != null:
+		#call("remove_collision_exception_with", server.permainan.dunia.get_node("pemain/"+str(id)))
+	#call("apply_central_force", Vector3(0, 25, 50).rotated(Vector3.UP, rotation.y))
+	if id == client.id_koneksi:
+		server.permainan.dunia.get_node("pemain/"+str(id))._atur_penarget(true)
+		server.permainan.get_node("kontrol_sentuh/aksi_1").visible = false
+		server.permainan.get_node("kontrol_sentuh/aksi_2").visible = false
+		server.permainan.get_node("hud/bantuan_input/aksi1").visible = false
+		server.permainan.get_node("hud/bantuan_input/aksi2").visible = false
+		
+		# reset pemroses pada server
+		var tmp_kondisi = [["id_proses", -1], ["id_pengemudi", -1]]
+		if server.permainan.koneksi == Permainan.MODE_KONEKSI.SERVER: server._sesuaikan_kondisi_entitas(id_proses, name, tmp_kondisi)
+		else: server.rpc_id(1, "_sesuaikan_kondisi_entitas", id_proses, name, tmp_kondisi)
+	# atur ulang id_pengemudi dan id_proses
+	id_pengemudi = -1
+	id_proses = -1
