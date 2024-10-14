606be1d94e6ebd6e639180a4fd9eb536c76c8175
zinzui12345 <48501018+zinzui12345@users.noreply.github.com>
2024-10-14 21:06:22 +0800

    fix bajaj accelerate sound

--- skrip/entitas/bajay.gd
+++ skrip/entitas/bajay.gd
@@ -54,8 +54,11 @@ var id_pengemudi = -1:
 				#$AudioMesin.stream.set_clip_auto_advance(1, AudioStreamInteractive.AUTO_ADVANCE_ENABLED) # ini belum bisa, jadi pake $AudioHentikanMesin
 				$AudioHentikanMesin.play()
 				$AudioMesin.stop()
-				$AudioAkselerasiMesin.volume_db = -30
-				$AudioAkselerasiMesin.stop()
+				var tween_volume_akselerasi = get_tree().create_tween()
+				tween_volume_akselerasi.tween_property($AudioAkselerasiMesin, "volume_db", -30, 1)
+				tween_volume_akselerasi.tween_property($AudioAkselerasiMesin, "pitch_scale", 0.7, 1)
+				tween_volume_akselerasi.tween_callback($AudioAkselerasiMesin.stop)
+				tween_volume_akselerasi.play()
 				
 				# aktifkan fisik dengan pengemudi
 				call("remove_collision_exception_with", server.permainan.dunia.get_node("pemain/"+str(id_pengemudi)))
@@ -82,15 +85,15 @@ var mat3 : StandardMaterial3D
 var mat4 : StandardMaterial3D
 var mat5 : StandardMaterial3D
 
-var warna_1 : Color :
+var warna_1 : Color = Color("FFF"):
 	set(ubah_warna):
 		if mat1 != null: mat1.albedo_color = ubah_warna
 		warna_1 = ubah_warna
-var warna_2 : Color :
+var warna_2 : Color = Color("3900ff"):
 	set(ubah_warna):
 		if mat2 != null: mat2.albedo_color = ubah_warna
 		warna_2 = ubah_warna
-var warna_3 : Color :
+var warna_3 : Color = Color("ff0021"):
 	set(ubah_warna):
 		if mat3 != null: mat3.albedo_color = ubah_warna
 		warna_3 = ubah_warna
@@ -192,6 +195,9 @@ func hapus(): # ketika tampilan dihapus
 func _kemudikan(id):
 	# atur pengemudi
 	if id == client.id_koneksi:
+		server.permainan.dunia.raycast_occlusion_culling.add_exception(self)
+		server.permainan.dunia.raycast_occlusion_culling.add_exception(server.permainan.dunia.get_node("pemain/"+str(id)))
+		
 		server.permainan.dunia.get_node("pemain/"+str(id))._atur_penarget(false)
 		server.permainan.dunia.get_node("pemain/"+str(id)+"/pengamat").atur_mode(2)
 		await get_tree().create_timer(0.05).timeout		# ini untuk mencegah fungsi !_target di _process()
@@ -204,7 +210,7 @@ func _kemudikan(id):
 		
 		# atur arah pemain pada mode VR
 		if server.permainan.mode_vr and server.permainan.pengamat_vr != null:
-			$arah_pengamat_vr.global_rotation = server.permainan.pengamat_vr.global_rotation
+			$arah_pengamat_vr.global_rotation.y = server.permainan.pengamat_vr.global_rotation.y
 			if server.permainan.pengamat_vr.get_node("XRCamera3D").rotation_degrees.y > 0:
 				var arah_putar = $arah_pengamat_vr.rotation_degrees.y + absf(server.permainan.pengamat_vr.get_node("XRCamera3D").rotation_degrees.y)
 				if $arah_pengamat_vr.rotation_degrees.y < 0:
@@ -229,6 +235,9 @@ func _kemudikan(id):
 func _lepas(id):
 	# atur pengemudi
 	if id == client.id_koneksi:
+		server.permainan.dunia.raycast_occlusion_culling.remove_exception(self)
+		server.permainan.dunia.raycast_occlusion_culling.remove_exception(server.permainan.dunia.get_node("pemain/"+str(id)))
+		
 		server.permainan.dunia.get_node("pemain/"+str(id)).rotation.x = 0
 		server.permainan.dunia.get_node("pemain/"+str(id)).rotation.z = 0
 		
@@ -252,14 +261,14 @@ func _lepas(id):
 	id_pengemudi = -1
 	id_proses = -1
 func _atur_audio_mesin(delta : float):
-	if self.engine_force > 0:
+	if self.engine_force > 0 and kecepatan_laju > 0:
 		if !$AudioAkselerasiMesin.playing:
 			$AudioAkselerasiMesin.playing = true
 		elif $AudioAkselerasiMesin.volume_db < 1:
 			$AudioAkselerasiMesin.volume_db = lerp($AudioAkselerasiMesin.volume_db, 1.0, 1.25 * delta)
 		if $AudioPeringatan.playing: $AudioPeringatan.playing = false
-		$AudioAkselerasiMesin.pitch_scale = clamp(kecepatan_laju / 7, 0.7, 2.7)
-	elif self.engine_force < 0:
+		$AudioAkselerasiMesin.pitch_scale = lerpf($AudioAkselerasiMesin.pitch_scale, clamp(kecepatan_laju / 7, 0.7, 2.7), 1.25 * delta)
+	elif self.engine_force < 0 and kecepatan_laju < 0:
 		if !$AudioAkselerasiMesin.playing:
 			$AudioAkselerasiMesin.playing = true
 		elif $AudioAkselerasiMesin.volume_db < 0.5:
@@ -268,9 +277,9 @@ func _atur_audio_mesin(delta : float):
 		if !$AudioPeringatan.playing and kecepatan_laju < -1: $AudioPeringatan.playing = true
 	else:
 		if $AudioAkselerasiMesin.playing:
-			if $AudioAkselerasiMesin.volume_db > -30:
-				$AudioAkselerasiMesin.volume_db = lerp($AudioAkselerasiMesin.volume_db, -30.0, delta)
-				$AudioAkselerasiMesin.pitch_scale = lerp($AudioAkselerasiMesin.pitch_scale, 1.0, delta)
+			if $AudioAkselerasiMesin.volume_db > -10:
+				$AudioAkselerasiMesin.volume_db = lerp($AudioAkselerasiMesin.volume_db, -10.0, 0.5 * delta)
+				$AudioAkselerasiMesin.pitch_scale = lerp($AudioAkselerasiMesin.pitch_scale, 0.7, 0.5 * delta)
 			else:
 				$AudioAkselerasiMesin.playing = false
 		if $AudioPeringatan.playing: $AudioPeringatan.playing = false
