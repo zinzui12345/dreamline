034d0a09a975252744b0d2035ac2c480c98f26bc
zinzui12345 <48501018+zinzui12345@users.noreply.github.com>
2024-11-06 21:44:49 +0700

    fixed untracked files

--- addons/awesome_input_icons/resources/scripts/configuration.gd
+++
@@ -1,3 +0,0 @@
-extends Resource
-class_name InputIconConfiguration
-@export var scheme: InputIconScheme
--- addons/panku_console/modules/setelan/env.gd
+++
@@ -1,4 +0,0 @@
-var _module:PankuModule
-
-func buka_setelan_debug(): 		_module.open_settings_window()
-func buka_setelan_permainan(): 	_module.buka_jendela_setelan()
