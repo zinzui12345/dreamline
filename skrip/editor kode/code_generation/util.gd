3be666a686091b22d9bedb86d4b17b6b2c2cbf11
zinzui12345 <48501018+zinzui12345@users.noreply.github.com>
2025-01-21 18:57:47 +0800

    fix resource  on export

--- skrip/editor kode/code_generation/util.gd
+++ skrip/editor kode/code_generation/util.gd
@@ -3,7 +3,7 @@ extends Object
 
 static func get_files_in_dir_recursive(path: String, pattern: String) -> Array:
 	var files = []
-	var dir := DirAccess.open(path) # FIXME : DirAccess cuman tersedia di Editor!
+	var dir := DirAccess.open(path) # FIXME : karena optimasi DirAccess cuman bisa akses resource di Editor. untuk mengatasinya cek disini : https://forum.godotengine.org/t/any-way-to-pick-a-file-from-a-folder-in-the-project-at-random/71086/3
 
 	if not dir:
 		return files
@@ -11,14 +11,18 @@ static func get_files_in_dir_recursive(path: String, pattern: String) -> Array:
 	dir.list_dir_begin()
 
 	var file_name = dir.get_next()
-
+	print_debug("membuka direktori : "+path)
+	
 	while file_name != "":
 		var file_path = path + "/" + file_name
+		print_debug("membuka file : "+file_path)
 		if dir.current_is_dir():
 			files.append_array(get_files_in_dir_recursive(file_path, pattern))
 		elif file_name.matchn(pattern):
 			files.append(file_path)
 
 		file_name = dir.get_next()
+
+	print_debug("apakah file pada direktori ditemukan? : "+str(files != []))
 
 	return files
