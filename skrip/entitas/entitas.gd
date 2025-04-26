d1f2e06af361e7ae7bdc48500f9d012207b696fb
zinzui12345 <48501018+zinzui12345@users.noreply.github.com>
2025-04-26 23:32:32 +0800

    add function to synchronize state manually

--- skrip/entitas/entitas.gd
+++ skrip/entitas/entitas.gd
@@ -34,41 +34,42 @@ func _setup() -> void:
 		rotasi_awal = global_rotation
 		mulai()
 
-func _sinkronkan_perubahan_kondisi() -> void:
+func sinkronkan_perubahan_kondisi(perubahan_kondisi : Array = []) -> void:
 	# buat variabel pembanding
-	var perubahan_kondisi : Array = []
 	var sinkron_kondisi : Array = get("sinkron_kondisi")
 	
 	# cek apakah kondisi sebelumnya telah tersimpan
 	if cek_kondisi.get("posisi") == null:	cek_kondisi["posisi"] = Vector3.ZERO
 	if cek_kondisi.get("rotasi") == null:	cek_kondisi["rotasi"] = rotation
 	
-	# cek apakah kondisi berubah
-	if global_position.y < server.permainan.batas_bawah:
-		# reset kondisi jika entitas jatuh ke void
-		perubahan_kondisi.append(["position", posisi_awal])
-		perubahan_kondisi.append(["rotation", rotasi_awal])
-		global_transform.origin = posisi_awal
-		rotation		 		= rotasi_awal
-	else:
-		if cek_kondisi["posisi"] != position:	perubahan_kondisi.append(["position", position])
-		if cek_kondisi["rotasi"] != rotation:	perubahan_kondisi.append(["rotation", rotation])
-	
-	# 24/04/25 :: buat array kondisi kustom
-	var daftar_kondisi_kustom : Array = []
-	# cek kondisi properti kustom
-	for p in sinkron_kondisi.size():
-		# cek apakah kondisi sebelumnya telah tersimpan
-		if cek_kondisi.get(sinkron_kondisi[p][0]) == null:	cek_kondisi[sinkron_kondisi[p][0]] = sinkron_kondisi[p][1]
-		
+	# 26/04/25 :: hanya cek kondisi pada pemroses jika properti perubahan_kondisi kosong
+	if id_proses == client.id_koneksi and perubahan_kondisi.size() == 0:
 		# cek apakah kondisi berubah
-		if cek_kondisi[sinkron_kondisi[p][0]] != get(sinkron_kondisi[p][0]):	daftar_kondisi_kustom.append([sinkron_kondisi[p][0], get(sinkron_kondisi[p][0])])
-	# 24/04/25 :: sinkronkan kondisi kustom yang berubah
-	if daftar_kondisi_kustom.size() > 0:	perubahan_kondisi.append(["kondisi", daftar_kondisi_kustom])
+		if global_position.y < server.permainan.batas_bawah:
+			# reset kondisi jika entitas jatuh ke void
+			perubahan_kondisi.append(["position", posisi_awal])
+			perubahan_kondisi.append(["rotation", rotasi_awal])
+			global_transform.origin = posisi_awal
+			rotation		 		= rotasi_awal
+		else:
+			if cek_kondisi["posisi"] != position:	perubahan_kondisi.append(["position", position])
+			if cek_kondisi["rotasi"] != rotation:	perubahan_kondisi.append(["rotation", rotation])
+		
+		# 24/04/25 :: buat array kondisi kustom
+		var daftar_kondisi_kustom : Array = []
+		# cek kondisi properti kustom
+		for p in sinkron_kondisi.size():
+			# cek apakah kondisi sebelumnya telah tersimpan
+			if cek_kondisi.get(sinkron_kondisi[p][0]) == null:	cek_kondisi[sinkron_kondisi[p][0]] = sinkron_kondisi[p][1]
+			
+			# cek apakah kondisi berubah
+			if cek_kondisi[sinkron_kondisi[p][0]] != get(sinkron_kondisi[p][0]):	daftar_kondisi_kustom.append([sinkron_kondisi[p][0], get(sinkron_kondisi[p][0])])
+		# 24/04/25 :: sinkronkan kondisi kustom yang berubah
+		if daftar_kondisi_kustom.size() > 0:	perubahan_kondisi.append(["kondisi", daftar_kondisi_kustom])
 	
 	# jika kondisi berubah, maka sinkronkan perubahan ke server
 	if perubahan_kondisi.size() > 0 and (is_instance_valid(server.peer) or is_instance_valid(client.peer)):
-		if client.id_koneksi == 1:
+		if server.permainan.koneksi == Permainan.MODE_KONEKSI.SERVER:
 			server._sesuaikan_kondisi_entitas(id_proses, name, perubahan_kondisi)
 		else:
 			server.rpc_id(1, "_sesuaikan_kondisi_entitas", id_proses, name, perubahan_kondisi)
@@ -85,7 +86,7 @@ func _sinkronkan_perubahan_kondisi() -> void:
 func mulai() -> void:
 	pass
 # fungsi yang akan dipanggil setiap saat menggantikan _process(delta)
-# bisa juga menggunakan _process(delta) tetapi kondisi harus di sinkronkan dengan memanggil _sinkronkan_perubahan_kondisi()
+# bisa juga menggunakan _process(delta) tetapi kondisi harus di sinkronkan secara manual dengan memanggil sinkronkan_perubahan_kondisi()
 func proses(_waktu_delta : float) -> void:
 	pass
 # fungsi yang akan dipanggil ketika id_proses diubah
@@ -100,7 +101,7 @@ func _process(delta : float) -> void:
 	if !is_instance_valid(server.permainan): set_process(false); return
 	
 	# sinkronkan perubahan kondisi
-	if id_proses == client.id_koneksi: _sinkronkan_perubahan_kondisi()
+	if id_proses == client.id_koneksi: sinkronkan_perubahan_kondisi()
 	
 	# panggil fungsi pemroses node
 	proses(delta)
